c70708f273fb232ece3b1f4100210d1be8bd13ea
khris <chrisuzor7@yahoo.com>
2022-11-08 08:45:46 +0100

    done

---
+++ js newjava.js
@@ -0,0 +1,4 @@
+document.getElementById('intro').innerHTML="My name is  christopher";
+document.getElementById('about').innerHTML="i'm a footballer and a code-learner";
+document.getElementById('body').innerHTML="08138497366";
+document.getElementById('contact').innerHTML="chrisuzor7@yahoo.com";
